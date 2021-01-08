9b60989eb2488735772f844996235e4d883c8866
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2021-01-08 18:28:17 -0500

    implemented routeGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { ErrorComponent } from './error/error.component';
 import { ListToDosComponent } from './list-to-dos/list-to-dos.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'welcome/:name', component: WelcomeComponent },
-  { path: 'todos', component: ListToDosComponent },
-  { path: 'logout', component: LogoutComponent },
+  { path: 'welcome/:name', component: WelcomeComponent, canActivate: [RouteGuardService] },
+  { path: 'todos', component: ListToDosComponent, canActivate: [RouteGuardService] },
+  { path: 'logout', component: LogoutComponent, canActivate: [RouteGuardService] },
   { path: '**', component: ErrorComponent }
 
 ];
