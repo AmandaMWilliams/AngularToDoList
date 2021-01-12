4102566762368662d160fe090a4d1e52bc8a384c
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2021-01-12 17:13:08 -0500

    Created a button for Custom welcome message

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -10,11 +11,17 @@ export class WelcomeComponent implements OnInit {
   name= ''
   message = 'some welcome message'
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute, 
+    private service:WelcomeDataService) { }
 
   ngOnInit(){
-    console.log(this.message)
+    //console.log(this.message)
     this.name= this.route.snapshot.params['name']
   }
 
+  getWelcomeMessage(){
+    this.service.executeHelloWorldBeanService();
+   // console.log("get welcome message")
+  }
 }
