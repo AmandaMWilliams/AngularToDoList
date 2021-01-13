ddad5aa9715bbfb8ba874abfa54c9f6b9dce5f1b
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2021-01-12 20:05:43 -0500

    Connected FtB ToDo items, successfully connecting

--- src/app/list-to-dos/list-to-dos.component.ts
+++ src/app/list-to-dos/list-to-dos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 export class Todo {
   constructor(
@@ -16,23 +17,31 @@ export class Todo {
 })
 export class ListToDosComponent implements OnInit {
 
-  todos = [
-    new Todo(1, 'Find a Job', false, new Date()),
-    new Todo(2, 'Buy a House', false, new Date()),
-    new Todo(3, 'Learn ALL the Code!', false, new Date())
+  todos: Todo[];
+   //= [
+  //   new Todo(1, 'Find a Job', false, new Date()),
+  //   new Todo(2, 'Buy a House', false, new Date()),
+  //   new Todo(3, 'Learn ALL the Code!', false, new Date())
     // { id: 1, description: 'Learn to Dance' },
     // { id: 2, description: 'Learn to Cook' },
     // { id: 3, description: 'Learn to Clean' }
-  ]
+ // ]
 
   // todo = {
   //   id: 1,
   //   description: 'Learn to Dance'
   // }
 
-  constructor() { }
+  constructor(
+    private todoService: TodoDataService
+  ) { }
 
   ngOnInit() {
+    this.todoService.retrieveAllTodos('amandaw').subscribe(
+      response => {
+        this.todos = response;
+      }
+    )
   }
 
 }
