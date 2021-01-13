a62e69a0be7233dbd79e3bc0c6870888f9164139
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2021-01-12 19:14:25 -0500

    added Path Variable to welcome message

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -20,13 +20,27 @@ export class WelcomeComponent implements OnInit {
     //console.log(this.message)
     this.name= this.route.snapshot.params['name']
   }
-
+  
   getWelcomeMessage(){
-    console.log(this.service.executeHelloWorldBeanService());
+    //console.log(this.service.executeHelloWorldBeanService());
     this.service.executeHelloWorldBeanService().subscribe(
-      response => this.handleSuccessfulResponse(response)
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
     );
    // console.log("get welcome message")
+  }
+
+  getWelcomeMessageWithParameter(){
+    //console.log(this.service.executeHelloWorldBeanService());
+    this.service.executeHelloWorldServiceWithPathVariable(this.name).subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+   // console.log("get welcome message")
+  }
+
+  handleErrorResponse(error){
+    this.welcomeMessageFromService = error.error.message
   }
 
   handleSuccessfulResponse(response){
