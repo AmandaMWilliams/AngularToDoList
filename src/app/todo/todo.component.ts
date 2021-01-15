a7a354bc5c3a3dc47b11b596cf5ab933ecabb4a9
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-01-15 17:14:15 -0500

    added routing to todo.comp.ts for updating todo

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Todo } from '../list-to-dos/list-to-dos.component';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 @Component({
   selector: 'app-todo',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
+  id: number
+  todo: Todo
 
-  constructor() { }
+  constructor(
+    private todoService: TodoDataService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.todoService.retrieveTodo('amandaw', this.id).subscribe(
+      data => this.todo = data
+    )
   }
 
 }
