9689c000329b43f4b3877b275647dc7786546d52
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-01-20 11:32:54 -0500

    Added Create todo functionality

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -19,20 +19,35 @@ export class TodoComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+
     this.id = this.route.snapshot.params['id'];
-    this.todo = new Todo(1, '', false, new Date());
-    this.todoService.retrieveTodo('amandaw', this.id).subscribe(
-      data => this.todo = data
-    )
+
+    this.todo = new Todo(this.id, '', false, new Date());
+
+    if (this.id != -1) {
+      this.todoService.retrieveTodo('amandaw', this.id).subscribe(
+        data => this.todo = data
+      )
+    }
   }
 
   saveToDo() {
-    this.todoService.updateTodo('amandaw', this.id, this.todo)
-      .subscribe(
-        data => {
-          console.log(data)
-          this.router.navigate(['todos'])
-        }
-      )
+    if (this.id === -1) {
+      this.todoService.createTodo('amandaw', this.todo)
+        .subscribe(
+          data => {
+            console.log(data)
+            this.router.navigate(['todos'])
+          }
+        )
+    } else {
+      this.todoService.updateTodo('amandaw', this.id, this.todo)
+        .subscribe(
+          data => {
+            console.log(data)
+            this.router.navigate(['todos'])
+          }
+        )
+    }
   }
 }
