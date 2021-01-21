e01044461cef979bf1d5196510e44aa030145a8b
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-01-21 13:32:17 -0500

    refactored HttpInterceptor to use Basic Authentication Token

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -20,23 +20,27 @@ export class WelcomeDataService {
   }
 
   executeHelloWorldServiceWithPathVariable(name) {
-    let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
-    let headers = new HttpHeaders({
-      Authorization: basicAuthHeaderString
-    })
+    // let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
+    // let headers = new HttpHeaders({
+    //   Authorization: basicAuthHeaderString
+    // })
 
     return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`,
-     {headers});
+    //  {headers}
+     )
+  ;
   
     //console.log("Execute Hello World Bean Service")
   }
 
-    createBasicAuthenticationHttpHeader() {
-      let username = 'amandaw'
-      let password = 'noodles'
-      let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
-      return basicAuthHeaderString;
-    }
-    //Access to XMLHttpRequest at 'http://localhost:8080/hello-world/path-variable/amandaw' from origin 'http://localhost:4200' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+    // createBasicAuthenticationHttpHeader() {
+    //   let username = 'amandaw'
+    //   let password = 'noodles'
+    //   let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+    //   return basicAuthHeaderString;
+    // }
+    //Access to XMLHttpRequest at 'http://localhost:8080/hello-world/path-variable/amandaw' 
+    // from origin 'http://localhost:4200' has been blocked by CORS policy: 
+    // No 'Access-Control-Allow-Origin' header is present on the requested resource.
   
 }
