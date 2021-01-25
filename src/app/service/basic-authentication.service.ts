90f9cb0e22811fab023e5a00886f66feecedad9c
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-01-25 16:48:29 -0500

    switched hardcoded user authentication to JWT User authentication

--- src/app/service/basic-authentication.service.ts
+++ src/app/service/basic-authentication.service.ts
@@ -23,6 +23,24 @@ export class BasicAuthenticationService {
   //   return false;
   // }
 
+  executeJWTAuthenticationService(username, password) {
+
+    return this.http.post<any>
+      (`${API_URL}/authenticate`, {
+        username,
+        password
+      }).pipe(
+          map(
+            data => {
+              sessionStorage.setItem(AUTHENTICATED_USER, username)
+              sessionStorage.setItem(TOKEN, `Bearer ${data.token}`)
+              return data;
+            }
+          )
+        );
+
+    //console.log("Execute Hello World Bean Service")
+  }
   executeAuthenticationService(username, password) {
     let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
 
